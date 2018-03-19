5742c3c30965fda6e8f8efb0d33787b493fd57a7
Andrew Dixon <spinlock99@users.noreply.github.com>
2018-03-19 19:54:52 +0000

    updated icon

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "NAME",
+      title: "todo icon test",
       template: "src/index.ejs"
     }),
     new OfflinePlugin()
